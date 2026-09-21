c1df28b1da3fcbca06fa837f531ca13fc6396fa2
Hammed Adetola <abiodunadetola11@gmail.com>
2026-09-21 10:43:18 +0100

    edited my code

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
 
 def index(request):
-    print(request.user)
-    # Processing - database, cache, renderingHTML template
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # get the 5 most recent questions from the database
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]  # ASC
+    # output = ", ".join([q.question_text for q in latest_question_list])
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
